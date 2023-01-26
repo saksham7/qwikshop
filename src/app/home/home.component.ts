d8f8fdf8be2b4f6b3917ea2dfc126e45c5053a51
Saksham <saksham@gmail.com>
2023-01-26 13:36:11 +0530

    changes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import SwiperCore, { Navigation, Pagination, Scrollbar, A11y, Autoplay, SwiperOptions } from 'swiper';
 SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
+import Swal from 'sweetalert2'
+declare var $: any;
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
@@ -28,4 +30,13 @@ export class HomeComponent implements OnInit {
     }
   };
   title = 'app12';
+
+  submitButton(){
+    Swal.fire(
+      'Good job!',
+      'You clicked the button!',
+      'success'
+    ),
+    ($('#loginPage') as any).modal('hide');
+  }
 }
